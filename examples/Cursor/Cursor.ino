9af7b43bdd92833521ec6d521990a84683857e70
Martino Facchin <m.facchin@arduino.cc>
2017-08-10 10:22:21 +0200

    Fix examples
    During the various rebases a \n got lost :(

--- examples/Cursor/Cursor.ino
+++ examples/Cursor/Cursor.ino
@@ -43,7 +43,8 @@
 #include <LiquidCrystal.h>
 
 // initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected toconst int rs=12, en=11, d4=5, d5=4, d6=3, d7=2;
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
