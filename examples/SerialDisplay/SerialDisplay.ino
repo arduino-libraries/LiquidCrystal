46f8b7c4e26a810a11347cdb8cc90dc807986592
drf5n <drf@vims.edu>
2023-02-07 12:14:17 -0500

    Update SerialDisplay.ino

--- examples/SerialDisplay/SerialDisplay.ino
+++ examples/SerialDisplay/SerialDisplay.ino
@@ -34,7 +34,8 @@
 
  This example code is in the public domain.
 
- http://www.arduino.cc/en/Tutorial/LiquidCrystalSerialDisplay
+ https://docs.arduino.cc/learn/electronics/lcd-displays#display-example
+ https://github.com/arduino-libraries/LiquidCrystal
 
 */
 
